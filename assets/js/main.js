a2cd22cd1bb1e51c739940f8e22051f9ef309bea
Lintang Hannan M <lintanghannan123@gmail.com>
2021-08-11 18:36:35 +0700

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,8 +19,25 @@ if(navClose){
 /* Validate if constant exists */
 
 
-/*==================== ACCORDION SKILLS ====================*/
+/*==================== SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+        skillsHeader = document.querySelectorAll('.skills__header')
 
+function toggleskills(params) {
+    let itemClass = this.parentNode.className
+
+    for(i=0; i< skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+
+    if (itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleskills)
+})
 
 /*==================== QUALIFICATION TABS ====================*/
 
